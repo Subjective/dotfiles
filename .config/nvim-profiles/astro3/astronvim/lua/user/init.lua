f9fd0d9c80e4a6aef9b750426ed853a0fedb6438
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-01 00:27:54 -0800

    feat(neo-tree): add trash keymap

--- .config/nvim-profiles/astro3/astronvim/lua/user/init.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/init.lua
@@ -230,7 +230,7 @@ local config = {
 			},
 			H = {
 				function()
-					require("astronvim.utils.buffer").nav( -(vim.v.count > 0 and vim.v.count or 1))
+					require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
 				end,
 				desc = "Previous buffer",
 			},
@@ -299,6 +299,179 @@ local config = {
 		--     luasnip.filetype_extend("javascript", { "javascriptreact" })
 		--   end,
 		-- },
+
+		-- TODO Split-up configuration and move neo-tree config to dedicated plugins directory
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			config = function(plugins)
+				-- TODO move after neo-tree improves (https://github.com/nvim-neo-tree/neo-tree.nvim/issues/707)
+				local global_commands = {
+					system_open = function(state)
+						require("astronvim.utils").system_open(state.tree:get_node():get_id())
+					end,
+					parent_or_close = function(state)
+						local node = state.tree:get_node()
+						if (node.type == "directory" or node:has_children()) and node:is_expanded() then
+							state.commands.toggle_node(state)
+						else
+							require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+						end
+					end,
+					child_or_open = function(state)
+						local node = state.tree:get_node()
+						if node.type == "directory" or node:has_children() then
+							if not node:is_expanded() then -- if unexpanded, expand
+								state.commands.toggle_node(state)
+							else -- if expanded and has children, seleect the next child
+								require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+							end
+						else -- if not a directory just open it
+							state.commands.open(state)
+						end
+					end,
+					copy_selector = function(state)
+						local node = state.tree:get_node()
+						local filepath = node:get_id()
+						local filename = node.name
+						local modify = vim.fn.fnamemodify
+
+						local results = {
+							e = { val = modify(filename, ":e"), msg = "Extension only" },
+							f = { val = filename, msg = "Filename" },
+							F = { val = modify(filename, ":r"), msg = "Filename w/o extension" },
+							h = { val = modify(filepath, ":~"), msg = "Path relative to Home" },
+							p = { val = modify(filepath, ":."), msg = "Path relative to CWD" },
+							P = { val = filepath, msg = "Absolute path" },
+						}
+
+						local messages = {
+							{ "\nChoose to copy to clipboard:\n", "Normal" },
+						}
+						for i, result in pairs(results) do
+							if result.val and result.val ~= "" then
+								vim.list_extend(messages, {
+									{ ("%s."):format(i), "Identifier" },
+									{ (" %s: "):format(result.msg) },
+									{ result.val, "String" },
+									{ "\n" },
+								})
+							end
+						end
+						vim.api.nvim_echo(messages, false, {})
+						local result = results[vim.fn.getcharstr()]
+						if result and result.val and result.val ~= "" then
+							vim.notify("Copied: " .. result.val)
+							vim.fn.setreg("+", result.val)
+						end
+					end,
+					trash = function(state)
+						local inputs = require("neo-tree.ui.inputs")
+						local path = state.tree:get_node().path
+						local msg = "Are you sure you want to trash " .. path
+						inputs.confirm(msg, function(confirmed)
+							if not confirmed then
+								return
+							end
+
+							vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+							require("neo-tree.sources.manager").refresh(state.name)
+						end)
+					end,
+					trash_visual = function(state, selected_nodes)
+						local inputs = require("neo-tree.ui.inputs")
+
+						function GetTableLen(tbl)
+							local len = 0
+							for _ in pairs(tbl) do
+								len = len + 1
+							end
+							return len
+						end
+
+						local count = GetTableLen(selected_nodes)
+						local msg = "Are you sure you want to trash " .. count .. " files ?"
+						inputs.confirm(msg, function(confirmed)
+							if not confirmed then
+								return
+							end
+							for _, node in ipairs(selected_nodes) do
+								vim.fn.system({ "trash", vim.fn.fnameescape(node.path) })
+							end
+							require("neo-tree.sources.manager").refresh(state.name)
+						end)
+					end,
+				}
+				local get_icon = require("astronvim.utils").get_icon
+				local opts = {
+					close_if_last_window = true,
+					source_selector = {
+						winbar = true,
+						content_layout = "center",
+						tab_labels = {
+							filesystem = get_icon("FolderClosed") .. " File",
+							buffers = get_icon("DefaultFile") .. " Bufs",
+							git_status = get_icon("Git") .. " Git",
+							diagnostics = get_icon("Diagnostic") .. " Diagnostic",
+						},
+					},
+					default_component_configs = {
+						indent = { padding = 0, indent_size = 1 },
+						icon = {
+							folder_closed = get_icon("FolderClosed"),
+							folder_open = get_icon("FolderOpen"),
+							folder_empty = get_icon("FolderEmpty"),
+							default = get_icon("DefaultFile"),
+						},
+						modified = { symbol = get_icon("FileModified") },
+						git_status = {
+							symbols = {
+								added = get_icon("GitAdd"),
+								deleted = get_icon("GitDelete"),
+								modified = get_icon("GitChange"),
+								renamed = get_icon("GitRenamed"),
+								untracked = get_icon("GitUntracked"),
+								ignored = get_icon("GitIgnored"),
+								unstaged = get_icon("GitUnstaged"),
+								staged = get_icon("GitStaged"),
+								conflict = get_icon("GitConflict"),
+							},
+						},
+					},
+					window = {
+						width = 30,
+						mappings = {
+							["<space>"] = false, -- disable space until we figure out which-key disabling
+							["[b"] = "prev_source",
+							["]b"] = "next_source",
+							o = "open",
+							O = "system_open",
+							h = "parent_or_close",
+							l = "child_or_open",
+							Y = "copy_selector",
+							T = "trash",
+						},
+					},
+					filesystem = {
+						follow_current_file = true,
+						hijack_netrw_behavior = "open_current",
+						use_libuv_file_watcher = true,
+						commands = global_commands,
+					},
+					buffers = { commands = global_commands },
+					git_status = { commands = global_commands },
+					diagnostics = { commands = global_commands },
+					event_handlers = {
+						{
+							event = "neo_tree_buffer_enter",
+							handler = function(_)
+								vim.opt_local.signcolumn = "auto"
+							end,
+						},
+					},
+				}
+				require("neo-tree").setup(opts)
+			end,
+		},
 		-- {
 		--   "windwp/nvim-autopairs",
 		--   config = function(plugin, opts)
@@ -357,6 +530,20 @@ local config = {
 			lazy = false,
 			priority = 1000,
 		},
+		-- {
+		-- 	"folke/todo-comments.nvim",
+		-- 	cmd = { "TodoTrouble", "TodoTelescope" },
+		-- 	event = { "BufReadPost", "BufNewFile" },
+		-- 	config = true,
+		-- 	-- stylua: ignore
+		-- 	keys = {
+		-- 		{ "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+		-- 		{ "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+		-- 		{ "<leader>xt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
+		-- 		{ "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
+		-- 		{ "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
+		-- 	},
+		-- },
 		{
 			"kylechui/nvim-surround",
 			lazy = false,
@@ -395,8 +582,8 @@ local config = {
 		{
 			"ggandor/leap.nvim",
 			keys = {
-				{ "s",  mode = { "n", "x", "o" }, desc = "Leap forward to" },
-				{ "S",  mode = { "n", "x", "o" }, desc = "Leap backward to" },
+				{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+				{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
 				{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
 			},
 			config = function(_, opts)
