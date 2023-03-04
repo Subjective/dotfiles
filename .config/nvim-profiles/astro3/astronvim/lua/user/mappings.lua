5208b4f75d3fa1de478ac106667373dc8d16de26
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-04 00:21:18 -0800

    feat(neo-tree): add two distinct mappings for toggling explorer
    "<leader>e" toggles explorer and sets root to cwd
    "<leader>E" toggles explorer and sets root to project directory
    "@" sets explorer root to home
    "." sets cwd and explorer root to selected tree node

--- .config/nvim-profiles/astro3/astronvim/lua/user/mappings.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/mappings.lua
@@ -17,6 +17,56 @@ return {
 		["<leader>b"] = { name = "Buffers" },
 		-- quick save
 		-- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+		["<leader>E"] = {
+			function()
+				local function get_root()
+					local root_patterns = { ".git", "lua" }
+					---@type string?
+					local path = vim.api.nvim_buf_get_name(0)
+					path = path ~= "" and vim.loop.fs_realpath(path) or nil
+					---@type string[]
+					local roots = {}
+					if path then
+						for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+							local workspace = client.config.workspace_folders
+							local paths = workspace
+									and vim.tbl_map(function(ws)
+										return vim.uri_to_fname(ws.uri)
+									end, workspace)
+								or client.config.root_dir and { client.config.root_dir }
+								or {}
+							for _, p in ipairs(paths) do
+								local r = vim.loop.fs_realpath(p)
+								if path:find(r, 1, true) then
+									roots[#roots + 1] = r
+								end
+							end
+						end
+					end
+					table.sort(roots, function(a, b)
+						return #a > #b
+					end)
+					---@type string?
+					local root = roots[1]
+					if not root then
+						path = path and vim.fs.dirname(path) or vim.loop.cwd()
+						---@type string?
+						root = vim.fs.find(root_patterns, { path = path, upward = true })[1]
+						root = root and vim.fs.dirname(root) or vim.loop.cwd()
+					end
+					---@cast root string
+					return root
+				end
+				require("neo-tree.command").execute({ toggle = true, dir = get_root() })
+			end,
+			desc = "Toggle Explorer (root)",
+		},
+		["<leader>e"] = {
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+			end,
+			desc = "Toggle Explorer (cwd)",
+		},
 		["<leader>ue"] = {
 			function()
 				local utils = require("astronvim.utils")
