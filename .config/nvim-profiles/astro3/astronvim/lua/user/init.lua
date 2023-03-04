c4722176a024fea3a198540c05067ea27b1ea7e4
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-03 19:15:28 -0800

    move polish function to separate file

--- .config/nvim-profiles/astro3/astronvim/lua/user/init.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/init.lua
@@ -117,24 +117,6 @@ local config = {
 			},
 		},
 	},
-	-- This function is run last and is a good place to configuring
-	-- augroups/autocommands and custom filetypes also this just pure lua so
-	-- anything that doesn't fit in the normal config locations above can go here
-	polish = function()
-		-- Set up custom filetypes
-		-- vim.filetype.add {
-		--   extension = {
-		--     foo = "fooscript",
-		--   },
-		--   filename = {
-		--     ["Foofile"] = "fooscript",
-		--   },
-		--   pattern = {
-		--     ["~/%.config/foo/.*"] = "fooscript",
-		--   },
-		-- }
-		--     { "BufEnter", "Filetype" },
-	end,
 }
 
 return config
