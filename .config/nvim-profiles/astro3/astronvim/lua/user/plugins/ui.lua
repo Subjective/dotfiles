667b2dc938b12a8e80915df3cd764edc9d6744ec
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-05 22:40:38 -0800

    load catppuccin on startup

--- .config/nvim-profiles/astro3/astronvim/lua/user/plugins/ui.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/plugins/ui.lua
@@ -1,18 +1,23 @@
 return {
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	config = function()
-	-- 		require("tokyonight").setup({
-	-- 			style = "night",
-	-- 			dim_inactive = true,
-	-- 		})
-	-- 	end,
-	-- },
 	{
-		"AstroNvim/astrocommunity",
-		{ import = "astrocommunity.colorscheme.tokyonight" },
-		{ import = "astrocommunity.colorscheme.catppuccin" },
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				-- dim_inactive = true,
+			})
+		end,
 	},
+	{
+		lazy = false,
+		"catppuccin/nvim",
+		name = "catppuccin",
+	},
+	-- {
+	-- 	"AstroNvim/astrocommunity",
+	-- 	{ import = "astrocommunity.colorscheme.tokyonight" },
+	-- 	{ import = "astrocommunity.colorscheme.catppuccin" },
+	-- },
 	{
 		"nvim-notify",
 		opts = {
@@ -177,17 +182,17 @@ return {
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
+							["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+							["vim.lsp.util.stylize_markdown"] = true,
+							["cmp.entry.get_documentation"] = true,
 				},
 			},
 			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
+				bottom_search = true,     -- use a classic bottom cmdline for search
+				command_palette = true,   -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = true, -- add a border to hover docs and signature help
+				inc_rename = false,       -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true,    -- add a border to hover docs and signature help
 			},
 		},
 		init = function()
