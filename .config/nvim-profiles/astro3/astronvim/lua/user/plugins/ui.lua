0c2ba8bf55b54735f3e684c5a13af6e057b823f0
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-06 16:01:08 -0800

    use default nvim-notify options

--- .config/nvim-profiles/astro3/astronvim/lua/user/plugins/ui.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/plugins/ui.lua
@@ -19,19 +19,6 @@ return {
 	-- 	{ import = "astrocommunity.colorscheme.catppuccin" },
 	-- },
 	{
-		"nvim-notify",
-		opts = {
-			stages = "fade_in_slide_out",
-			timeout = 3000,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-		},
-	},
-	{
 		"karb94/neoscroll.nvim",
 		event = "VeryLazy",
 		config = function()
@@ -182,17 +169,17 @@ return {
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
-							["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-							["vim.lsp.util.stylize_markdown"] = true,
-							["cmp.entry.get_documentation"] = true,
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
 				},
 			},
 			presets = {
-				bottom_search = true,     -- use a classic bottom cmdline for search
-				command_palette = true,   -- position the cmdline and popupmenu together
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = true,    -- add a border to hover docs and signature help
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
 			},
 		},
 		init = function()
