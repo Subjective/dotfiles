c7697ef894db7b07b3f6a74298148dc57c51eff8
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-07 20:15:44 -0800

    add polish.lua

---
+++ .config/nvim-profiles/astro3/astronvim/lua/user/polish.lua
@@ -0,0 +1,20 @@
+-- This function is run last and is a good place to configuring
+-- augroups/autocommands and custom filetypes also this just pure lua so
+-- anything that doesn't fit in the normal config locations above can go here
+return function()
+	-- Set up custom filetypes
+	-- vim.filetype.add {
+	--   extension = {
+	--     foo = "fooscript",
+	--   },
+	--   filename = {
+	--     ["Foofile"] = "fooscript",
+	--   },
+	--   pattern = {
+	--     ["~/%.config/foo/.*"] = "fooscript",
+	--   },
+	-- }
+	--     { "BufEnter", "Filetype" },
+	-- vim.env.GIT_WORK_TREE = vim.fn.expand("~")
+	-- vim.env.GIT_DIR = vim.fn.expand("~/.cfg")
+end
