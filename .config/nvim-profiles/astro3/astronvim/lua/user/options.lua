2a24b787a559a547b40ef3bccfcf60eae988a2c1
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-11 00:39:09 -0800

    enable spellcheck by default

--- .config/nvim-profiles/astro3/astronvim/lua/user/options.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/options.lua
@@ -4,7 +4,7 @@ return {
 		-- set to true or false etc.
 		relativenumber = true, -- sets vim.opt.relativenumber
 		number = true, -- sets vim.opt.number
-		spell = false, -- sets vim.opt.spell
+		spell = true, -- sets vim.opt.spell
 		signcolumn = "auto", -- sets vim.opt.signcolumn to auto
 		wrap = false, -- sets vim.opt.wrap
 	},
