7dceb22c56ae7534efca9ff18dee8afb7981b676
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-11 00:52:29 -0800

    disable lua-ls formatting

--- .config/nvim-profiles/astro3/astronvim/lua/user/init.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/init.lua
@@ -46,7 +46,7 @@ local config = {
 				},
 			},
 			disabled = { -- disable formatting capabilities for the listed language servers
-				-- "lua_ls",
+				"lua_ls",
 			},
 			timeout_ms = 1000, -- default format timeout
 			-- filter = function(client) -- fully override the default formatting function
