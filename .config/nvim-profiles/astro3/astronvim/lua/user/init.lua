98e69cb27e033281087aae60207de32d8b86f7bc
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-11 15:21:05 -0800

    split lsp config into separate files

--- .config/nvim-profiles/astro3/astronvim/lua/user/init.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/init.lua
@@ -28,68 +28,25 @@ local config = {
 	diagnostics = {
 		underline = true,
 	},
-	-- Extend LSP configuration
-	lsp = {
-		-- enable servers that you already have installed without mason
-		servers = {
-			-- "pyright"
-		},
-		formatting = {
-			-- control auto formatting on save
-			format_on_save = {
-				enabled = true, -- enable or disable format on save globally
-				allow_filetypes = { -- enable format on save for specified filetypes only
-					-- "go",
-				},
-				ignore_filetypes = { -- disable format on save for specified filetypes
-					-- "python",
-				},
-			},
-			disabled = { -- disable formatting capabilities for the listed language servers
-				"lua_ls",
-			},
-			timeout_ms = 1000, -- default format timeout
-			-- filter = function(client) -- fully override the default formatting function
-			--   return true
-			-- end
-		},
-		-- easily add or disable built in mappings added during LSP attaching
-		mappings = {
-			n = {
-				-- ["<leader>lf"] = false -- disable formatting keymap
-			},
-		},
-		-- add to the global LSP on_attach function
-		-- on_attach = function(client, bufnr)
-		-- end,
-
-		-- override the LSP setup handler function based on server name
-		-- setup_handlers = {
-		--   -- first function changes the default setup handler
-		--   function(server, opts) require("lspconfig")[server].setup(opts) end,
-		--   -- keys for a specific server name will be used for that LSP
-		--   lua_ls = function(server, opts)
-		--     -- custom lua_ls setup handler
-		--     require("lspconfig")["lua_ls"].setup(opts)
-		--   end,
-		-- },
-
-		-- Add overrides for LSP server settings, the keys are the name of the server
-		config = {
-			-- example for addings schemas to yamlls
-			-- yamlls = { -- override table for require("lspconfig").yamlls.setup({...})
-			--   settings = {
-			--     yaml = {
-			--       schemas = {
-			--         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-			--         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-			--         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-			--       },
-			--     },
-			--   },
-			-- },
-		},
-	},
+	-- -- Extend LSP configuration
+	-- lsp = {
+	--
+	-- 	-- Add overrides for LSP server settings, the keys are the name of the server
+	-- 	config = {
+	-- 		-- example for addings schemas to yamlls
+	-- 		-- yamlls = { -- override table for require("lspconfig").yamlls.setup({...})
+	-- 		--   settings = {
+	-- 		--     yaml = {
+	-- 		--       schemas = {
+	-- 		--         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+	-- 		--         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+	-- 		--         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+	-- 		--       },
+	-- 		--     },
+	-- 		--   },
+	-- 		-- },
+	-- 	},
+	-- },
 	-- Customize Heirline options
 	heirline = {
 		-- -- Customize different separators between sections
