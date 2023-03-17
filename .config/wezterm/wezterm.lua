5b18b2d71ee309cb74adc5e71b5dacb1327f4223
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-17 02:31:41 -0700

    update wezterm defaults

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,20 +4,20 @@ return {
 	-- term="wezterm",
 	-- font = wezterm.font("Cica"),
 	-- font_size = 10.0,
-	font = wezterm.font("FiraCode Nerd Font"),
-	font_size = 12,
+	font = wezterm.font({ family = "FiraCode Nerd Font" }),
+	font_size = 16,
 	-- cell_width = 1.1,
 	-- line_height = 1.1,
 	-- font_rules = {
 	-- 	{
 	-- 		italic = true,
-	-- 		font = wezterm.font("Cica", { italic = true }),
+	-- 		font = wezterm.font("FiraCode Nerd Font", { italic = true }),
 	-- 	},
-	-- 	{
-	-- 		italic = true,
-	-- 		intensity = "Bold",
-	-- 		font = wezterm.font("Cica", { weight = "Bold", italic = true }),
-	-- 	},
+	-- {
+	-- 	italic = true,
+	-- 	intensity = "Bold",
+	-- 	font = wezterm.font("Cica", { weight = "Bold", italic = true }),
+	-- },
 	-- },
 	check_for_updates = true,
 	use_ime = true,
@@ -31,7 +31,8 @@ return {
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
 	cursor_blink_rate = 0,
-	-- enable_wayland = enable_wayland(),
+	force_reverse_video_cursor = true,
+	-- enable_wayland = enable_wayland(),,
 	-- https://github.com/wez/wezterm/issues/1772
 	enable_wayland = false,
 	color_scheme = "tokyonight_night",
