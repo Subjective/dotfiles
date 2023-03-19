04d66c7eabd01e1256d4df3c673e80fd237b6c23
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-19 05:00:47 -0700

    add icon to diff view menu in which-key

--- .config/nvim-profiles/astro3/astronvim/lua/user/plugins/diffview.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/plugins/diffview.lua
@@ -9,7 +9,7 @@ return {
 
 		utils.set_mappings({
 			n = {
-				[prefix] = { name = "Diff View" },
+				[prefix] = { name = " Diff View" },
 				[prefix .. "<cr>"] = { "<cmd>DiffviewOpen<cr>", desc = "Open DiffView" },
 				[prefix .. "h"] = { "<cmd>DiffviewFileHistory %<cr>", desc = "Open DiffView File History" },
 				[prefix .. "H"] = { "<cmd>DiffviewFileHistory<cr>", desc = "Open DiffView Branch History" },
@@ -20,17 +20,17 @@ return {
 			local out = {}
 			local i = 1
 			for lhs, def in
-				pairs(utils.extend_tbl(maps, {
-					[prefix .. "q"] = { "<cmd>DiffviewClose<cr>", desc = "Quit Diffview" }, -- Toggle the file panel.
-					["]D"] = { actions.select_next_entry, desc = "Next Difference" }, -- Open the diff for the next file
-					["[D"] = { actions.select_prev_entry, desc = "Previous Difference" }, -- Open the diff for the previous file
-					["[C"] = { actions.prev_conflict, desc = "Next Conflict" }, -- In the merge_tool: jump to the previous conflict
-					["]C"] = { actions.next_conflict, desc = "Previous Conflict" }, -- In the merge_tool: jump to the next conflict
-					["Cl"] = { actions.cycle_layout, desc = "Cycle Diff Layout" }, -- Cycle through available layouts.
-					["Ct"] = { actions.listing_style, desc = "Cycle Tree Style" }, -- Cycle through available layouts.
-					["<leader>e"] = { actions.toggle_files, desc = "Toggle Explorer" }, -- Toggle the file panel.
-					["<leader>o"] = { actions.focus_files, desc = "Focus Explorer" }, -- Bring focus to the file panel
-				}))
+			pairs(utils.extend_tbl(maps, {
+				[prefix .. "q"] = { "<cmd>DiffviewClose<cr>", desc = "Quit Diffview" }, -- Toggle the file panel.
+				["]D"] = { actions.select_next_entry, desc = "Next Difference" },    -- Open the diff for the next file
+				["[D"] = { actions.select_prev_entry, desc = "Previous Difference" }, -- Open the diff for the previous file
+				["[C"] = { actions.prev_conflict, desc = "Next Conflict" },          -- In the merge_tool: jump to the previous conflict
+				["]C"] = { actions.next_conflict, desc = "Previous Conflict" },      -- In the merge_tool: jump to the next conflict
+				["Cl"] = { actions.cycle_layout, desc = "Cycle Diff Layout" },       -- Cycle through available layouts.
+				["Ct"] = { actions.listing_style, desc = "Cycle Tree Style" },       -- Cycle through available layouts.
+				["<leader>e"] = { actions.toggle_files, desc = "Toggle Explorer" },  -- Toggle the file panel.
+				["<leader>o"] = { actions.focus_files, desc = "Focus Explorer" },    -- Bring focus to the file panel
+			}))
 			do
 				local opts
 				local rhs = def
@@ -60,7 +60,7 @@ return {
 					[prefix .. "o"] = { actions.conflict_choose("ours"), desc = "Take Ours" }, -- Choose the OURS version of a conflict
 					[prefix .. "t"] = { actions.conflict_choose("theirs"), desc = "Take Theirs" }, -- Choose the THEIRS version of a conflict
 					[prefix .. "b"] = { actions.conflict_choose("base"), desc = "Take Base" }, -- Choose the BASE version of a conflict
-					[prefix .. "a"] = { actions.conflict_choose("all"), desc = "Take All" }, -- Choose all the versions of a conflict
+					[prefix .. "a"] = { actions.conflict_choose("all"), desc = "Take All" },  -- Choose all the versions of a conflict
 					[prefix .. "0"] = { actions.conflict_choose("none"), desc = "Take None" }, -- Delete the conflict region
 				}),
 				diff3 = build_keymaps({
@@ -73,19 +73,19 @@ return {
 					[prefix .. "T"] = { actions.diffget("theirs"), mode = { "n", "x" }, desc = "Get Their Diff" }, -- Obtain the diff hunk from the THEIRS version of the file
 				}),
 				file_panel = build_keymaps({
-					j = actions.next_entry, -- Bring the cursor to the next file entry
-					k = actions.prev_entry, -- Bring the cursor to the previous file entry.
+					j = actions.next_entry,                            -- Bring the cursor to the next file entry
+					k = actions.prev_entry,                            -- Bring the cursor to the previous file entry.
 					o = actions.select_entry,
-					S = actions.stage_all, -- Stage all entries.
-					U = actions.unstage_all, -- Unstage all entries.
-					X = actions.restore_entry, -- Restore entry to the state on the left side.
-					L = actions.open_commit_log, -- Open the commit log panel.
+					S = actions.stage_all,                             -- Stage all entries.
+					U = actions.unstage_all,                           -- Unstage all entries.
+					X = actions.restore_entry,                         -- Restore entry to the state on the left side.
+					L = actions.open_commit_log,                       -- Open the commit log panel.
 					Cf = { actions.toggle_flatten_dirs, desc = "Flatten" }, -- Flatten empty subdirectories in tree listing style.
-					R = actions.refresh_files, -- Update stats and entries in the file list.
-					["-"] = actions.toggle_stage_entry, -- Stage / unstage the selected entry.
+					R = actions.refresh_files,                         -- Update stats and entries in the file list.
+					["-"] = actions.toggle_stage_entry,                -- Stage / unstage the selected entry.
 					["<down>"] = actions.next_entry,
 					["<up>"] = actions.prev_entry,
-					["<cr>"] = actions.select_entry, -- Open the diff for the selected entry.
+					["<cr>"] = actions.select_entry,   -- Open the diff for the selected entry.
 					["<2-LeftMouse>"] = actions.select_entry,
 					["<c-b>"] = actions.scroll_view(-0.25), -- Scroll the view up
 					["<c-f>"] = actions.scroll_view(0.25), -- Scroll the view down
