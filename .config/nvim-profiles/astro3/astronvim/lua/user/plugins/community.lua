91db0a525974363ec2430d1552db31ba5d2e1ed9
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-03-26 14:38:39 -0700

    (astro): move community configs to dedicated file

---
+++ .config/nvim-profiles/astro3/astronvim/lua/user/plugins/community.lua
@@ -0,0 +1,18 @@
+return {
+	"AstroNvim/astrocommunity",
+	-- language packs
+	{ import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+
+	-- colorschemes
+	{ import = "astrocommunity.colorscheme.tokyonight" },
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.colorscheme.gruvbox" },
+
+	-- ui
+	{ import = "astrocommunity.markdown-and-latex.glow-nvim" },
+	{ import = "astrocommunity.editing-support.todo-comments-nvim" },
+	{ import = "astrocommunity.media.vim-wakatime" },
+	-- { import = "astrocommunity.indent.indent-blankline-nvim" },
+	-- { import = "astrocommunity.indent.mini-indentscope" },
+}
--- .config/nvim-profiles/astro3/astronvim/lua/user/plugins/ui.lua
+++ .config/nvim-profiles/astro3/astronvim/lua/user/plugins/ui.lua
@@ -1,13 +1,4 @@
 return {
-	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.colorscheme.tokyonight" },
-	{ import = "astrocommunity.colorscheme.catppuccin" },
-	{ import = "astrocommunity.colorscheme.gruvbox" },
-	{ import = "astrocommunity.markdown-and-latex.glow-nvim" },
-	{ import = "astrocommunity.editing-support.todo-comments-nvim" },
-	{ import = "astrocommunity.media.vim-wakatime" },
-	-- { import = "astrocommunity.indent.indent-blankline-nvim" },
-	-- { import = "astrocommunity.indent.mini-indentscope" },
 	{
 		lazy = false,
 		"tokyonight.nvim",
