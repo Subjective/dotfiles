524da063e92ed10a2606a446b85f57b00d92b5d5
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-04-10 15:28:11 -0700

    chore(bin): remove extraneous comments from nvim profile shell scripts

--- .local/bin/lazyvim.sh
+++ .local/bin/lazyvim.sh
@@ -4,13 +4,6 @@ NVIM_PROFILE_NAME="lazyvim"
 NVIM_INIT_FILE="init.lua"
 NVIM_COMMAND="nvim"
 
-# exec env XDG_CONFIG_HOME="$HOME/.config/nvim-profiles/${NVIM_PROFILE_NAME}" \
-#          XDG_DATA_HOME="$HOME/.local/share/nvim-profiles/${NVIM_PROFILE_NAME}" \
-#          XDG_STATE_HOME="$HOME/.local/state/nvim-profiles/${NVIM_PROFILE_NAME}" \
-#          XDG_CACHE_HOME="$HOME/.cache/nvim-profiles/${NVIM_PROFILE_NAME}" \
-#          ${NVIM_COMMAND} -u "$HOME/.config/nvim-profiles/${NVIM_PROFILE_NAME}/nvim/${NVIM_INIT_FILE}"\
-#          "$@"
-
 exec env XDG_CONFIG_HOME="$HOME/.config/nvim-profiles/${NVIM_PROFILE_NAME}" \
          XDG_DATA_HOME="$HOME/.local/share/nvim-profiles/${NVIM_PROFILE_NAME}" \
          XDG_STATE_HOME="$HOME/.local/state/nvim-profiles/${NVIM_PROFILE_NAME}" \
