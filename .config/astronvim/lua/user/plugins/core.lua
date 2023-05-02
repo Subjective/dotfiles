3cc9794ba4524b15b2894e33ae98356d68532c80
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-05-02 13:27:48 -0700

    chore(astro): update luasnip config comments

--- .config/astronvim/lua/user/plugins/core.lua
+++ .config/astronvim/lua/user/plugins/core.lua
@@ -37,8 +37,8 @@ return {
     config = function(plugin, opts)
       opts.enable_autosnippets = true
       require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      require("luasnip.loaders.from_vscode").lazy_load { paths = { "~/.config/astronvim/lua/user/snippets/vscode" } } -- load snippets paths
-      require("luasnip.loaders.from_lua").load { paths = "~/.config/astronvim/lua/user/snippets/lua" }
+      require("luasnip.loaders.from_vscode").lazy_load { paths = { "~/.config/astronvim/lua/user/snippets/vscode" } } -- load vscode snippets
+      require("luasnip.loaders.from_lua").load { paths = "~/.config/astronvim/lua/user/snippets/lua" } -- load lua snippets
     end,
   },
 }
