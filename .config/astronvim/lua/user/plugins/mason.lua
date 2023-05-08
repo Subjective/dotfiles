304660ec0db989f61f09c3d3a14ae9b95d500dfc
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-05-08 02:01:56 -0700

    chore(astro): remove unecessary variables and eslint_d

--- .config/astronvim/lua/user/plugins/mason.lua
+++ .config/astronvim/lua/user/plugins/mason.lua
@@ -27,11 +27,10 @@ return {
     "jay-babu/mason-null-ls.nvim",
     config = function(_, opts)
       local mason_null_ls = require "mason-null-ls"
-      local null_ls = require "null-ls"
       -- Ensure that opts.ensure_installed exists and is a table.
       if not opts.ensure_installed then opts.ensure_installed = {} end
       -- Add to opts.ensure_installed using vim.list_extend.
-      utils.list_insert_unique(opts.ensure_installed, { "stylua", "eslint_d" })
+      utils.list_insert_unique(opts.ensure_installed, { "stylua" })
       -- Remove lsps added by an astrocommunity language pack
       utils.list_remove(opts.ensure_installed, { "rustywind" })
       -- run setup
