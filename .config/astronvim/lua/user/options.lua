21940bb27877e8b134250da50d37f35f75dc1eb7
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-05-08 23:40:04 -0700

    fix(astro): tabline autohide flicker upon opening single buffer

--- .config/astronvim/lua/user/options.lua
+++ .config/astronvim/lua/user/options.lua
@@ -8,7 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     conceallevel = 2,
-    -- showtabline = 0,
+    showtabline = 0,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
