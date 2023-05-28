7436f9337e6931bf81a72c70b3c7a03d7cdf9c35
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-05-27 17:17:02 -0700

    feat(p10k): change catppuccin prompt symbol from blue to magenta

--- .p10k/themes/p10k-catppuccin.zsh
+++ .p10k/themes/p10k-catppuccin.zsh
@@ -193,8 +193,8 @@
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_BACKGROUND=
-  # Green prompt symbol if the last command succeeded.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=4
+  # Magenta prompt symbol if the last command succeeded.
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=5
   # Red prompt symbol if the last command failed.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=1
   # Default prompt symbol.
