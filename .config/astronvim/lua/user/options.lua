fd8d0f90338c94f82756c474967d1dd21a4ad3fc
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-04 15:23:24 -0700

    fix(astro): always show signcolumn to prevent text from shifting

--- .config/astronvim/lua/user/options.lua
+++ .config/astronvim/lua/user/options.lua
@@ -5,7 +5,6 @@ return {
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     conceallevel = 2,
     showtabline = 0,
