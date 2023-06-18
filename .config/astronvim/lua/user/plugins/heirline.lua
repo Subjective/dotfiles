11ec273b587c77eae2851048d3db5307c1d1b310
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-17 20:57:53 -0700

    chore(astro): cleanup extraneous comments in heirline.lua

--- .config/astronvim/lua/user/plugins/heirline.lua
+++ .config/astronvim/lua/user/plugins/heirline.lua
@@ -3,19 +3,15 @@ return {
   opts = function(_, opts)
     local status = require "astronvim.utils.status"
     opts.statusline = {
-      -- statusline
       hl = { fg = "fg", bg = "bg" },
-      -- status.component.mode({ mode_text = { padding = { left = 1, right = 1 } } }), -- add the mode text
       status.component.mode(),
       status.component.git_branch(),
       status.component.git_diff(),
       status.component.diagnostics(),
       status.component.file_info {
         filetype = false,
-        -- filename = { fallback = "Empty" },
         filename = false,
         file_modified = false,
-        -- file_modified = { padding = { left = 0 } },
         file_read_only = false,
         surround = { separator = "none" },
       },
@@ -39,7 +35,6 @@ return {
       status.component.nav(),
       status.component.mode { surround = { separator = "right" } },
     }
-    -- opts.winbar = nil
     return opts
   end,
 }
