e6501f9111cf25e5e43a8f02f1b18371a3ad21b6
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-18 16:42:00 -0700

    fix(astro): don't open cmdline when accepting codeium suggestion

--- .config/astronvim/lua/user/plugins/ai.lua
+++ .config/astronvim/lua/user/plugins/ai.lua
@@ -11,7 +11,7 @@ return {
       vim.g.codeium_idle_delay = 1500
     end,
     config = function()
-      vim.keymap.set("i", "<C-;>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<C-;>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
       vim.keymap.set("i", "<C-->", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
       vim.keymap.set("i", "<C-=>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
       vim.keymap.set("i", "<C-BS>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
