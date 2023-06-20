aada0bd0f89f7cf1cc4a521b6f2dc17730cb695b
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-19 17:44:09 -0700

    Revert "chore(astro): remove unecessary ensure_installed table for treesitter"
    This reverts commit 03a5dfad147851e970590299ba970e74ede9cfe9.

--- .config/astronvim/lua/user/plugins/treesitter.lua
+++ .config/astronvim/lua/user/plugins/treesitter.lua
@@ -1,3 +1,5 @@
+local utils = require "astronvim.utils"
+
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
@@ -5,6 +7,36 @@ return {
     { "RRethy/nvim-treesitter-textsubjects" },
   },
   opts = function(_, opts)
+    -- Ensure that opts.ensure_installed exists and is a table or string "all".
+    if not opts.ensure_installed then
+      opts.ensure_installed = {}
+    elseif opts.ensure_installed == "all" then
+      return
+    end
+    -- Add the required file types to opts.ensure_installed.
+    utils.list_insert_unique(opts.ensure_installed, {
+      "lua",
+      "javascript",
+      "typescript",
+      "swift",
+      "html",
+      "css",
+      "cpp",
+      "python",
+      "java",
+      "bash",
+      "markdown",
+      "markdown_inline",
+      "regex",
+      "vim",
+      "latex",
+      "tsx",
+      "diff",
+      "git_rebase",
+      "gitcommit",
+      "git_config",
+      "gitignore",
+    })
     opts.auto_install = vim.fn.executable "tree-sitter" == 1
     opts.matchup = { enable = true }
     opts.indent = { enable = true, disable = { "python" } }
