63d674cc63d9d998cb80289eb71e29aa252944a1
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-21 08:28:19 -0700

    feat(astro): create autocmd that maps q to quit fugitive buffer

--- .config/astronvim/lua/user/plugins/git.lua
+++ .config/astronvim/lua/user/plugins/git.lua
@@ -4,6 +4,14 @@ return {
   {
     "tpope/vim-fugitive",
     event = "User AstroGitFile",
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "Make q close fugitive",
+        group = vim.api.nvim_create_augroup("quit_fugitive", { clear = true }),
+        pattern = { "fugitive", "fugitiveblame" },
+        callback = function() vim.keymap.set("n", "q", "<Plug>fugitive:gq", { buffer = true }) end,
+      })
+    end,
   },
   {
     "pwntester/octo.nvim",
