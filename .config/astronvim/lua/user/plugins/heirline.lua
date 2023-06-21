97aa49d6ebd35b8216151fad410eb2521eed2d4f
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-20 22:40:06 -0700

    fix(astro): allow custom tag names for grapple heirline component

--- .config/astronvim/lua/user/plugins/heirline.lua
+++ .config/astronvim/lua/user/plugins/heirline.lua
@@ -6,7 +6,7 @@ return {
     status.component.grapple = {
       provider = function()
         local success, key = pcall(require("grapple").key, { buffer = 0 })
-        if success and key and key > 0 then
+        if success and key then
           return " " .. key .. " "
         else
           return
