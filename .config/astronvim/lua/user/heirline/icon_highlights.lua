b04e875b53f060a91d4d5dcb1e7341dfd8d12682
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-21 05:18:55 -0700

    feat(astro): disable highlighting for lsp symbols in breadcrumbs

--- .config/astronvim/lua/user/heirline/icon_highlights.lua
+++ .config/astronvim/lua/user/heirline/icon_highlights.lua
@@ -1,6 +1,6 @@
 -- -- Customize if icons should be highlighted
 return {
-  breadcrumbs = true, -- LSP symbols in the breadcrumbs
+  breadcrumbs = nil, -- LSP symbols in the breadcrumbs
   file_icon = {
     winbar = false, -- Filetype icon in the winbar inactive windows
     statusline = true, -- Filetype icon in the statusline
