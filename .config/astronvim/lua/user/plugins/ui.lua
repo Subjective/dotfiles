be7a6ce2ae0fb7bdceb105bfc30e749d550088f4
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-21 03:10:02 -0700

    feat(astro): use mini view for nvim-notify notifications

--- .config/astronvim/lua/user/plugins/ui.lua
+++ .config/astronvim/lua/user/plugins/ui.lua
@@ -76,7 +76,7 @@ return {
         -- The default routes will forward notifications to nvim-notify
         -- Benefit of using Noice for this is the routing and consistent history view
         enabled = true,
-        view = "notify",
+        view = "mini",
       },
       lsp = {
         progress = {
