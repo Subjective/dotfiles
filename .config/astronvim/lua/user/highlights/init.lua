c7effd7702b5e70b4ce7539e1064778e9f0060da
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-27 04:33:29 -0500

    chore(astro): cleanup highlights/init.lua

--- .config/astronvim/lua/user/highlights/init.lua
+++ .config/astronvim/lua/user/highlights/init.lua
@@ -1,18 +1,11 @@
 -- Add highlight groups in any theme
-return function()
-  local get_hlgroup = require("astronvim.utils").get_hlgroup
-  local nontext = get_hlgroup "NonText"
-  return {
-    -- init = { -- this table overrides highlights in all themes
-    --   Normal = { bg = "#000000" },
-    -- }
-    -- duskfox = { -- a table of overrides/changes to the duskfox theme
-    --   Normal = { bg = "#000000" },
-    -- },
-    CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
-    GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true }, -- italicize git blame virtual text
-    HighlightURL = { undercurl = true }, -- always underline URLs
-    OctoEditable = { fg = "NONE", bg = "NONE" }, -- use treesitter for octo.nvim highlighting
-    LeapBackdrop = { link = "Comment" }, -- gray out the search area when leaping
-  }
-end
+local get_hlgroup = require("astronvim.utils").get_hlgroup
+local nontext = get_hlgroup "NonText"
+
+return {
+  CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
+  GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true }, -- italicize git blame virtual text
+  HighlightURL = { undercurl = true }, -- always underline URLs
+  OctoEditable = { fg = "NONE", bg = "NONE" }, -- use treesitter for octo.nvim highlighting
+  LeapBackdrop = { link = "Comment" }, -- gray out the search area when leaping
+}
