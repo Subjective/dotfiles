04f69d87b4e6ba06ea43988d32501a4120529de1
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-01 14:51:02 -0500

    feat(astro): switch to flash.nvim from leap/flit

--- .config/astronvim/lua/user/plugins/editor.lua
+++ .config/astronvim/lua/user/plugins/editor.lua
@@ -7,20 +7,46 @@ return {
     config = function()
       require("nvim-surround").setup {
         indent_lines = false,
-        keymaps = {
-          insert = "<C-g>s",
-          insert_line = "<C-g>S",
-          normal = "ys",
-          normal_cur = "yss",
-          normal_line = "ys",
-          normal_cur_line = "ySS",
-          visual = "Z",
-          visual_line = "gZ",
-          delete = "ds",
-          change = "cs",
-        },
       }
     end,
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Toggle Flash Search",
+      },
+    },
   },
   {
     "folke/todo-comments.nvim",
