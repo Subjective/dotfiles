66270b46550eb68cfcaa102c0e767ace40ab88a2
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-02 04:12:04 -0500

    feat(astro): change leap highlight color from red to pink

--- .config/astronvim/lua/user/highlights/catppuccin-mocha.lua
+++ .config/astronvim/lua/user/highlights/catppuccin-mocha.lua
@@ -4,6 +4,6 @@ require("leap").opts.highlight_unlabeled_phase_one_targets = true
 return {
   -- lightspeed-style highlighting
   LeapMatch = { fg = mocha.text, bold = true, nocombine = true },
-  LeapLabelPrimary = { fg = mocha.red, bold = true, nocombine = true },
+  LeapLabelPrimary = { fg = mocha.pink, bold = true, nocombine = true },
   LeapLabelSecondary = { fg = mocha.blue, bold = true, nocombine = true },
 }
