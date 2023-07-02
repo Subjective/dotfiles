7a3780ddd40f42858ef398e7c40d88d000496efd
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-02 03:10:19 -0500

    Revert "feat(astro): update leap highlight groups"
    This reverts commit 88645ebcc0b96834651f5acdc9ed03e9db6f56f0.

--- .config/astronvim/lua/user/highlights/catppuccin-mocha.lua
+++ .config/astronvim/lua/user/highlights/catppuccin-mocha.lua
@@ -1,12 +1,9 @@
 local mocha = require("catppuccin.palettes").get_palette "mocha"
 require("leap").opts.highlight_unlabeled_phase_one_targets = true
 
-local get_hlgroup = require("astronvim.utils").get_hlgroup
-local comment = get_hlgroup "Comment"
-
 return {
   -- lightspeed-style highlighting
-  LeapMatch = { fg = mocha.text, bg = comment.fg, bold = true, nocombine = true },
-  LeapLabelPrimary = { fg = mocha.pink, bg = comment.fg, bold = true, nocombine = true },
-  LeapLabelSecondary = { fg = mocha.blue, bg = comment.fg, bold = true, nocombine = true },
+  LeapMatch = { fg = mocha.text, bold = true, nocombine = true },
+  LeapLabelPrimary = { fg = mocha.red, bold = true, nocombine = true },
+  LeapLabelSecondary = { fg = mocha.blue, bold = true, nocombine = true },
 }
