58deb6db9cdc8850769e8b052f02143f4dbcdf2d
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-04 05:32:39 -0500

    feat(astro): enable splitkeep and showbreak

--- .config/astronvim/lua/user/options.lua
+++ .config/astronvim/lua/user/options.lua
@@ -1,14 +1,13 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
-    -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    wrap = false, -- sets vim.opt.wrap
+    spell = false,
+    wrap = false,
     conceallevel = 2,
     swapfile = false,
     clipboard = "",
+    splitkeep = "screen",
+    showbreak = "↪ ",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
