7b14bbc7480d7a64c35dc189249c01eb69692558
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-05 12:44:46 -0700

    chore(astro): remove need for global dotfiles table in dotfiles_git autocmd

--- .config/astronvim/lua/user/autocmds.lua
+++ .config/astronvim/lua/user/autocmds.lua
@@ -26,19 +26,23 @@ autocmd({ "BufEnter" }, {
   group = augroup("dotfiles_git", { clear = true }),
   pattern = { "*" },
   callback = function(args)
-    if vim.fn.expand "%" == "" or vim.api.nvim_get_option_value("buftype", { buf = args.buf }) == "nofile" then return end
+    local filename = vim.api.nvim_buf_get_name(0)
+    if filename == "" or vim.api.nvim_get_option_value("buftype", { buf = args.buf }) == "nofile" then return end
 
     local home_dir = vim.env.HOME
 
-    if not vim.g.dotfile_list then
-      vim.g.dotfile_list =
-        vim.fn.system("git -C $HOME --work-tree=" .. home_dir .. " --git-dir=" .. home_dir .. "/.cfg ls-tree --name-only -r HEAD")
-    end
-
-    local filename = vim.api.nvim_buf_get_name(0)
-    local relative_filename = string.gsub(filename, home_dir .. "/", "", 1)
-
-    if vim.fn.index(vim.fn.split(vim.g.dotfile_list, "\n"), relative_filename) ~= -1 then
+    if
+      astro_utils.cmd({
+        "git",
+        "--work-tree",
+        home_dir,
+        "--git-dir",
+        home_dir .. "/.cfg",
+        "ls-files",
+        "--error-unmatch",
+        filename,
+      }, false)
+    then
       vim.env.GIT_DIR = home_dir .. "/.cfg"
       vim.env.GIT_WORK_TREE = home_dir
       if not is_available "gitsigns" then pcall(require, "gitsigns") end
