5e5c42489da46af6e86f2733188462782cde48a6
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-09 15:40:35 -0700

    feat(astro): configure nvchad-like chatgpt theme

--- .config/astronvim/lua/user/plugins/ai.lua
+++ .config/astronvim/lua/user/plugins/ai.lua
@@ -48,7 +48,37 @@ return {
         x = { ["<leader>;r"] = { name = "ChatGPT: Run" } },
       }
     end,
-    opts = {},
+    opts = {
+      popup_window = {
+        border = {
+          highlight = "TelescopePreviewBorder",
+        },
+        win_options = {
+          winhighlight = "Normal:TelescopePreviewNormal,FloatBorder:FloatBorder",
+        },
+      },
+      system_window = {
+        border = {
+          highlight = "TelescopePromptBorder",
+        },
+        win_options = {
+          winhighlight = "Normal:TelescopePromptNormal,FloatBorder:FloatBorder",
+        },
+      },
+      popup_input = {
+        border = {
+          highlight = "TelescopePromptBorder",
+        },
+        win_options = {
+          winhighlight = "Normal:TelescopePromptNormal,FloatBorder:FloatBorder",
+        },
+      },
+      settings_window = {
+        win_options = {
+          winhighlight = "Normal:TelescopePromptNormal,FloatBorder:FloatBorder",
+        },
+      },
+    },
     keys = {
       { "<leader>;c", "<cmd>ChatGPT<cr>", desc = "ChatGPT" },
       { "<leader>;a", "<cmd>ChatGPTActAs<cr>", desc = "ChatGPT: Act As" },
