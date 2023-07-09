36f2cca509a1f3a748dbb53c641b2726c7c27a8f
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-09 01:35:13 -0700

    fix(astro): add missing dependency for spectre.nvim

--- .config/astronvim/lua/user/plugins/editor.lua
+++ .config/astronvim/lua/user/plugins/editor.lua
@@ -44,6 +44,7 @@ return {
   {
     "nvim-pack/nvim-spectre",
     cmd = "Spectre",
+    dependencies = "nvim-lua/plenary.nvim",
     opts = function()
       local prefix = "<leader>s"
       return {
