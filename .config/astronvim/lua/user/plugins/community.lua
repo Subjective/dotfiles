8d07d65c03e0da92e909edcc053da8c6d0486884
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-10 04:58:31 -0700

    feat(astro): add flatten.nvim

--- .config/astronvim/lua/user/plugins/community.lua
+++ .config/astronvim/lua/user/plugins/community.lua
@@ -137,6 +137,32 @@ return {
     cmd = { "Portal" },
   },
 
+  -- terminal integration
+  { import = "astrocommunity.terminal-integration.flatten-nvim" },
+  {
+    "willothy/flatten.nvim",
+    opts = {
+      window = {
+        open = "alternate",
+      },
+      callbacks = {
+        post_open = function(bufnr, winnr, ft)
+          vim.api.nvim_set_current_win(winnr)
+          -- If the file is a git commit, create one-shot autocmd to delete its buffer on write
+          if ft == "gitcommit" then
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              buffer = bufnr,
+              once = true,
+              callback = function()
+                vim.defer_fn(function() vim.api.nvim_buf_delete(bufnr, {}) end, 50)
+              end,
+            })
+          end
+        end,
+      },
+    },
+  },
+
   -- markdown and latex
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   {
