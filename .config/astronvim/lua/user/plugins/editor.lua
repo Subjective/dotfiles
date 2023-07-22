b7e3ff78f9864362b0ea1cf549cb9256a9d11cbb
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-21 19:11:18 -0700

    feat(astro): simplify visual surround bindings

--- .config/astronvim/lua/user/plugins/editor.lua
+++ .config/astronvim/lua/user/plugins/editor.lua
@@ -4,7 +4,21 @@ return {
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "s",
+        visual_line = "S",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
+      },
+    },
   },
   {
     "folke/todo-comments.nvim",
