289bcae371117b99e383ac9750a3231dee806dd8
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-21 18:37:51 -0700

    chore(astro): remove unecessary surround keybinds and enable indent_lines

--- .config/astronvim/lua/user/plugins/editor.lua
+++ .config/astronvim/lua/user/plugins/editor.lua
@@ -4,22 +4,7 @@ return {
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
-    opts = {
-      indent_lines = false,
-      keymaps = {
-        insert = "<C-g>s",
-        insert_line = "<C-g>S",
-        normal = "ys",
-        normal_cur = "yss",
-        normal_line = "yS",
-        normal_cur_line = "ySS",
-        visual = "S",
-        visual_line = "gS",
-        delete = "ds",
-        change = "cs",
-        change_line = "cS",
-      },
-    },
+    opts = {},
   },
   {
     "folke/todo-comments.nvim",
