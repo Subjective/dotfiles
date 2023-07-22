5b8c0f4b96726fb0ada9818bbcb05ec99a2d4e39
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-22 13:38:08 -0700

    feat(astro): enable popup view for offscreen matches in vim-matchup

--- .config/astronvim/lua/user/plugins/treesitter.lua
+++ .config/astronvim/lua/user/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
       "andymass/vim-matchup",
       init = function()
         vim.g.matchup_matchparen_deferred = 1
-        vim.g.matchup_matchparen_offscreen = {}
+        vim.g.matchup_matchparen_offscreen = { method = "popup", fullwidth = 1, highlight = "Normal", syntax_hl = 1 }
       end,
     },
     { "Subjective/nvim-treesitter-textsubjects" },
