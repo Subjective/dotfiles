9763c360bae4644fbd137fce7a7339a4a63157cb
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-22 18:10:31 -0700

    fix(astro): missing validation field error when calling urlview.nvim

--- .config/astronvim/lua/user/plugins/editor.lua
+++ .config/astronvim/lua/user/plugins/editor.lua
@@ -150,7 +150,7 @@ return {
     dependencies = "nvim-telescope/telescope.nvim",
     opts = {},
     keys = {
-      { "<leader>fU", "<cmd>UrlView<cr>", desc = "Find URLs" },
+      { "<leader>fU", "<cmd>UrlView buffer bufnr=0<cr>", desc = "Find URLs" },
       { "<leader>fL", "<cmd>UrlView lazy<cr>", desc = "Find plugin URLs" },
     },
   },
