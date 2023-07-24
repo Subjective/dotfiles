39d340baa0ab6a9359403bc29c18ee67b876057b
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-24 05:44:40 -0700

    chore(astro): set "[No Name]" as title for no name files in tabline

--- .config/astronvim/lua/user/plugins/heirline.lua
+++ .config/astronvim/lua/user/plugins/heirline.lua
@@ -43,5 +43,36 @@ return {
       status.component.nav(),
       status.component.mode { surround = { separator = "right" } },
     }
+
+    opts.tabline = { -- bufferline
+      { -- file tree padding
+        condition = function(self)
+          self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
+          return status.condition.buffer_matches(
+            { filetype = { "aerial", "dapui_.", "dap-repl", "neo%-tree", "NvimTree", "edgy" } },
+            vim.api.nvim_win_get_buf(self.winid)
+          )
+        end,
+        provider = function(self) return string.rep(" ", vim.api.nvim_win_get_width(self.winid) + 1) end,
+        hl = { bg = "tabline_bg" },
+      },
+      status.heirline.make_buflist(status.component.tabline_file_info { filename = { fallback = "[No Name]" } }), -- component for each buffer tab
+      status.component.fill { hl = { bg = "tabline_bg" } }, -- fill the rest of the tabline with background color
+      { -- tab list
+        condition = function() return #vim.api.nvim_list_tabpages() >= 2 end, -- only show tabs if there are more than one
+        status.heirline.make_tablist { -- component for each tab
+          provider = status.provider.tabnr(),
+          hl = function(self) return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true) end,
+        },
+        { -- close button for current tab
+          provider = status.provider.close_button { kind = "TabClose", padding = { left = 1, right = 1 } },
+          hl = status.hl.get_attributes("tab_close", true),
+          on_click = {
+            callback = function() require("astronvim.utils.buffer").close_tab() end,
+            name = "heirline_tabline_close_tab_callback",
+          },
+        },
+      },
+    }
   end,
 }
