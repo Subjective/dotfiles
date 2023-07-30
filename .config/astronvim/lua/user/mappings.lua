ed75c0f072ca3e695a3a41eb47b449f69b6d0010
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-30 11:35:24 -0700

    refactor(astro): move explorer bindings into neotree config

--- .config/astronvim/lua/user/mappings.lua
+++ .config/astronvim/lua/user/mappings.lua
@@ -25,47 +25,7 @@ return {
     },
     -- explorer bindings
     ["<leader>o"] = false,
-    ["<leader>E"] = {
-      function()
-        local function get_root()
-          local root_patterns = { ".git", "lua" }
-          ---@type string?
-          local path = vim.api.nvim_buf_get_name(0)
-          path = path ~= "" and vim.uv.fs_realpath(path) or nil
-          ---@type string[]
-          local roots = {}
-          if path then
-            for _, client in pairs(vim.lsp.get_clients { bufnr = 0 }) do
-              local workspace = client.config.workspace_folders
-              local paths = workspace and vim.tbl_map(function(ws) return vim.uri_to_fname(ws.uri) end, workspace)
-                or client.config.root_dir and { client.config.root_dir }
-                or {}
-              for _, p in ipairs(paths) do
-                local r = vim.uv.fs_realpath(p)
-                if path:find(r, 1, true) then roots[#roots + 1] = r end
-              end
-            end
-          end
-          table.sort(roots, function(a, b) return #a > #b end)
-          ---@type string?
-          local root = roots[1]
-          if not root then
-            path = path and vim.fs.dirname(path) or vim.uv.cwd()
-            ---@type string?
-            root = vim.fs.find(root_patterns, { path = path, upward = true })[1]
-            root = root and vim.fs.dirname(root) or vim.uv.cwd()
-          end
-          ---@cast root string
-          return root
-        end
-        require("neo-tree.command").execute { toggle = true, dir = get_root() }
-      end,
-      desc = "Toggle Explorer (root)",
-    },
-    ["<leader>e"] = {
-      function() require("neo-tree.command").execute { toggle = true, dir = vim.uv.cwd() } end,
-      desc = "Toggle Explorer (cwd)",
-    },
+    ["<leader>e"] = false,
     -- toggle winbar
     ["<leader>uW"] = {
       function()
--- .config/astronvim/lua/user/plugins/neo-tree.lua
+++ .config/astronvim/lua/user/plugins/neo-tree.lua
@@ -1,4 +1,5 @@
 local utils = require "astronvim.utils"
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
@@ -55,5 +56,51 @@ return {
     })
     opts.filesystem.bind_to_cwd = false
     opts.default_component_configs.indent = { padding = 0, indent_size = 2 }
+  end,
+  keys = function()
+    local function get_root()
+      local root_patterns = { ".git", "lua" }
+      ---@type string?
+      local path = vim.api.nvim_buf_get_name(0)
+      path = path ~= "" and vim.uv.fs_realpath(path) or nil
+      ---@type string[]
+      local roots = {}
+      if path then
+        for _, client in pairs(vim.lsp.get_clients { bufnr = 0 }) do
+          local workspace = client.config.workspace_folders
+          local paths = workspace and vim.tbl_map(function(ws) return vim.uri_to_fname(ws.uri) end, workspace)
+            or client.config.root_dir and { client.config.root_dir }
+            or {}
+          for _, p in ipairs(paths) do
+            local r = vim.uv.fs_realpath(p)
+            if path:find(r, 1, true) then roots[#roots + 1] = r end
+          end
+        end
+      end
+      table.sort(roots, function(a, b) return #a > #b end)
+      ---@type string?
+      local root = roots[1]
+      if not root then
+        path = path and vim.fs.dirname(path) or vim.uv.cwd()
+        ---@type string?
+        root = vim.fs.find(root_patterns, { path = path, upward = true })[1]
+        root = root and vim.fs.dirname(root) or vim.uv.cwd()
+      end
+      ---@cast root string
+      return root
+    end
+
+    return {
+      {
+        "<leader>e",
+        function() require("neo-tree.command").execute { toggle = true, dir = vim.uv.cwd() } end,
+        desc = "Toggle Explorer (cwd)",
+      },
+      {
+        "<leader>E",
+        function() require("neo-tree.command").execute { toggle = true, dir = get_root() } end,
+        desc = "Toggle Explorer (root)",
+      },
+    }
   end,
 }
