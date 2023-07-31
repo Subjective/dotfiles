6dfccbb4b41698450de582f50e5b3597a8c57949
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-30 20:59:58 -0700

    chore(astro): use less obstructive listchar for tab

--- .config/astronvim/lua/user/options.lua
+++ .config/astronvim/lua/user/options.lua
@@ -7,7 +7,8 @@ return {
     clipboard = "",
     splitkeep = "screen",
     list = true, -- show whitespace characters
-    listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
+    listchars = { tab = "│·", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
+    -- listchars = { tab = "»·", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
     swapfile = false,
     spellfile = vim.fn.expand "~/.config/astronvim/lua/user/spell/en.utf-8.add",
     thesaurus = vim.fn.expand "~/.config/astronvim/lua/user/spell/mthesaur.txt",
