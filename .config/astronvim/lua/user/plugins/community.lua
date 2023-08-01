98b188f43a65857819845e8cfac1fc7e70b03b59
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-08-01 03:59:06 -0700

    fix(astro): make telescope and cmp selection color consistent

--- .config/astronvim/lua/user/plugins/community.lua
+++ .config/astronvim/lua/user/plugins/community.lua
@@ -48,6 +48,8 @@ return {
           TelescopePromptNormal = { fg = colors.text, bg = colors.crust },
           TelescopePromptBorder = { fg = colors.crust, bg = colors.crust },
           TelescopePromptPrefix = { fg = colors.flamingo, bg = colors.crust },
+          -- make telescope and cmp selection color consistent
+          PmenuSel = { bg = colors.surface0 },
         }
       end,
     },
