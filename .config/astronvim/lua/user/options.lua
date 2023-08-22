81c8b03a5d97219707b5020c16976d314f83eaef
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-08-21 17:34:41 -0700

    chore(astro): update outdated comment

--- .config/astronvim/lua/user/options.lua
+++ .config/astronvim/lua/user/options.lua
@@ -19,7 +19,7 @@ return {
     cmp_enabled = true, -- enable completion at start
     autopairs_enabled = true, -- enable autopairs at start
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available)
     ui_notifications_enabled = true, -- enable notifications
     resession_enabled = true, -- enable resession
     git_worktrees = { { toplevel = vim.env.HOME, gitdir = vim.env.HOME .. "/.cfg" } }, -- allow detached git_worktrees to be detected
