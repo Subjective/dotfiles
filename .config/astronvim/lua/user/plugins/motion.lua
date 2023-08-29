7b9d6646d13f73195f24003b8de05dfd16648176
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-08-29 13:50:13 -0700

    fix(astro): leap conflicting keymap with folding in visual mode
    See https://github.com/ggandor/leap.nvim/discussions/41

--- .config/astronvim/lua/user/plugins/motion.lua
+++ .config/astronvim/lua/user/plugins/motion.lua
@@ -5,10 +5,12 @@ return {
     keys = {
       { "s", "<Plug>(leap-forward-to)", mode = { "n" }, desc = "Leap forward to" },
       { "S", "<Plug>(leap-backward-to)", mode = { "n" }, desc = "Leap backward to" },
-      { "z", "<Plug>(leap-forward-to)", mode = { "x", "o" }, desc = "Leap forward to" },
-      { "Z", "<Plug>(leap-backward-to)", mode = { "x", "o" }, desc = "Leap backward to" },
-      { "x", "<Plug>(leap-forward-till)", mode = { "x", "o" }, desc = "Leap forward till" },
-      { "X", "<Plug>(leap-backward-till)", mode = { "x", "o" }, desc = "Leap backward till" },
+      { "z", "<Plug>(leap-forward-to)", mode = { "o" }, desc = "Leap forward to" },
+      { "Z", "<Plug>(leap-backward-to)", mode = { "o" }, desc = "Leap backward to" },
+      { "x", "<Plug>(leap-forward-till)", mode = { "o" }, desc = "Leap forward till" },
+      { "X", "<Plug>(leap-backward-till)", mode = { "o" }, desc = "Leap backward till" },
+      { "x", "<Plug>(leap-forward-to)", mode = { "x" }, desc = "Leap forward to" },
+      { "X", "<Plug>(leap-backward-to)", mode = { "x" }, desc = "Leap backward to" },
       { "gs", "<Plug>(leap-from-window)", mode = { "n" }, desc = "Leap from window" },
     },
     init = function() -- https://github.com/ggandor/leap.nvim/issues/70#issuecomment-1521177534
