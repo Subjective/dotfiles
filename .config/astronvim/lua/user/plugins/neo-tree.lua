de66e7f029504260a06805d2fb24d0adf6455ad7
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-08-30 16:08:29 -0700

    chore(astro): remove `smart_open` and `select_file` neotree commands

--- .config/astronvim/lua/user/plugins/neo-tree.lua
+++ .config/astronvim/lua/user/plugins/neo-tree.lua
@@ -28,49 +28,6 @@ return {
           require("neo-tree.sources.manager").refresh(state.name)
         end)
       end,
-      select_file = function(state)
-        state.clipboard = state.clipboard or {}
-        local function select_node(node)
-          if node.type == "directory" then
-            local children = state.tree:get_nodes(node:get_id())
-            for _, child in ipairs(children) do
-              select_node(child)
-            end
-          else
-            local id = node:get_id()
-            local data = state.clipboard[id]
-            if data and data.action == "select" then
-              state.clipboard[id] = nil
-            else
-              state.clipboard[id] = { action = "select", node = node }
-            end
-            require("neo-tree.ui.renderer").redraw(state)
-          end
-        end
-        local node = state.tree:get_node()
-        if node then
-          require("neo-tree.sources.filesystem.commands").expand_all_nodes(state, node)
-          vim.defer_fn(function() select_node(node) end, 100)
-        end
-      end,
-      smart_open = function(state)
-        local clipboard = state.clipboard or {}
-        local node = state.tree:get_node()
-        local selected = false
-        for id, data in pairs(clipboard) do
-          if data.action == "select" then
-            selected = true
-            require("neo-tree.utils").open_file(state, id)
-            state.clipboard[id] = nil
-          end
-        end
-        if selected then
-          if node.type ~= "directory" then require("neo-tree.utils").open_file(state, node:get_id()) end
-        else
-          state.commands["open"](state)
-        end
-        require("neo-tree.ui.renderer").redraw(state)
-      end,
       clear_clipboard = function(state)
         state.clipboard = {}
         require("neo-tree.ui.renderer").redraw(state)
@@ -82,8 +39,6 @@ return {
       ["~"] = "set_root_to_home",
       T = "trash",
       Z = "expand_all_nodes",
-      ["<tab>"] = "select_file",
-      ["<cr>"] = "smart_open",
       X = "clear_clipboard",
     })
     opts.window.mappings.o = nil -- TODO: remove in AstroNvim v4
