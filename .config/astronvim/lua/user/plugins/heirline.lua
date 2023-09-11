611aeef48559fda2f7c9fe2cb975879aaf79add8
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-09-11 14:53:03 -0700

    feat(astro): modularize special heirline components

--- .config/astronvim/lua/user/plugins/heirline.lua
+++ .config/astronvim/lua/user/plugins/heirline.lua
@@ -3,48 +3,56 @@ return {
   opts = function(_, opts)
     local status = require "astronvim.utils.status"
 
-    status.component.grapple = {
-      provider = function()
-        local available, grapple = pcall(require, "grapple")
-        if available then
-          if grapple.exists() then return string.format(" %s ", grapple.key()) end
-        end
-      end,
-      update = { "User", pattern = "GrappleStateUpdate", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
-      init = require("astronvim.utils.status.init").update_events { "BufEnter" },
-    }
+    status.component.grapple = function()
+      local available, grapple = pcall(require, "grapple")
+      return {
+        provider = function()
+          if available then
+            if grapple.exists() then return string.format(" %s ", grapple.key()) end
+          end
+        end,
+        update = { "User", pattern = "GrappleStateUpdate", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
+        init = require("astronvim.utils.status.init").update_events { "BufEnter" },
+      }
+    end
 
-    local ignored_buftypes = { "nofile", "prompt", "nowrite", "help", "quickfix" }
-    local ignored_filetypes = { "toggleterm" }
-    local function is_file()
-      return not status.condition.buffer_matches({ buftype = ignored_buftypes, filetype = ignored_filetypes }, 0)
-        or vim.fn.expand "%" == "[Command Line]"
+    status.component.smart_file_info = function()
+      local ignored_buftypes = { "nofile", "prompt", "nowrite", "help", "quickfix" }
+      local ignored_filetypes = { "toggleterm" }
+      local is_file = function()
+        return not status.condition.buffer_matches({ buftype = ignored_buftypes, filetype = ignored_filetypes }, 0)
+          or vim.fn.expand "%" == "[Command Line]"
+      end
+
+      return {
+        {
+          flexible = 10,
+          status.component.file_info {
+            filename = { fallback = "[No Name]", modify = ":~:." },
+            surround = { condition = is_file },
+          },
+          status.component.file_info {
+            filename = {},
+            surround = { condition = is_file },
+          },
+        },
+        status.component.file_info {
+          filetype = {},
+          filename = false,
+          file_icon = false,
+          file_read_only = {},
+          file_modified = false,
+          surround = { condition = function() return not is_file() end },
+        },
+      }
     end
 
     opts.statusline = {
       hl = { fg = "fg", bg = "bg" },
       status.component.mode(),
       status.component.git_branch(),
-      status.component.grapple,
-      {
-        flexible = 10,
-        status.component.file_info {
-          filename = { fallback = "[No Name]", modify = ":~:." },
-          surround = { condition = is_file },
-        },
-        status.component.file_info {
-          filename = {},
-          surround = { condition = is_file },
-        },
-      },
-      status.component.file_info {
-        filetype = {},
-        filename = false,
-        file_icon = false,
-        file_read_only = {},
-        file_modified = false,
-        surround = { condition = function() return not is_file() end },
-      },
+      status.component.grapple(),
+      status.component.smart_file_info(),
       status.component.git_diff(),
       status.component.diagnostics(),
       status.component.fill(),
