1d0a2ddc4f9b2ab3766f49f9b62a6621dde8b310
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-09-12 01:07:09 -0700

    feat(wezterm): add tmux multiplexing bindings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -37,7 +37,30 @@ return {
     "fish",
     "tmux",
   },
+  leader = { key = " ", mods = "CTRL" },
   keys = {
+    -- tmux
+    { key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+    { key = "\\", mods = "LEADER", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+    { key = "c", mods = "LEADER", action = wezterm.action.SpawnTab "CurrentPaneDomain" },
+    { key = "m", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+    { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Left" },
+    { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Down" },
+    { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Up" },
+    { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection "Right" },
+    { key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+    { key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+    { key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane { confirm = true } },
+    {
+      key = ",",
+      mods = "LEADER",
+      action = wezterm.action.PromptInputLine {
+        description = "Tab name",
+        action = wezterm.action_callback(function(window, _, line)
+          if line then window:active_tab():set_title(line) end
+        end),
+      },
+    },
     -- allow tmux to receive special bindings
     { key = "'", mods = "CTRL", action = wezterm.action.SendString "\x1b[27;5;39~" },
     { key = ";", mods = "CTRL", action = wezterm.action.SendString "\x1b[27;5;59~" },
