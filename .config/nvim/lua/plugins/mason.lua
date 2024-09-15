076845b49cbc0205d0273dd44d32900c5a4775ba
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2024-09-15 23:56:26 +0100

    fix(astro): "tsserver" renamed to "ts_ls"

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
-        "tsserver",
+        "ts_ls",
         "tailwindcss",
         "cssls",
         "vimls",
