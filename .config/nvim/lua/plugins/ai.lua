f1c07b11566cfa27a448ade165bf7db98b4de025
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2025-01-30 18:28:32 -0800

    feat(astro): configure codecompanion and remove ChatGPT.nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -41,107 +41,6 @@ return {
     },
   },
   {
-    "jackMort/ChatGPT.nvim",
-    cmd = { "ChatGPT", "ChatGPTActAs", "ChatGPTCompleteCode", "ChatGPTEditWithInstructions", "ChatGPTRun" },
-    init = function()
-      utils.set_mappings {
-        n = { ["<leader>;r"] = { name = "ChatGPT: Run" } },
-        x = { ["<leader>;r"] = { name = "ChatGPT: Run" } },
-      }
-    end,
-    opts = function()
-      require("cmp").setup.filetype({ "chatgpt-input" }, {
-        completion = {
-          autocomplete = false,
-        },
-      })
-      return {
-        popup_window = {
-          border = {
-            highlight = "TelescopePreviewBorder",
-          },
-          win_options = {
-            winhighlight = "Normal:TelescopePreviewNormal,FloatBorder:FloatBorder",
-          },
-        },
-        system_window = {
-          border = {
-            highlight = "TelescopePromptBorder",
-          },
-          win_options = {
-            winhighlight = "Normal:TelescopePromptNormal,FloatBorder:FloatBorder",
-          },
-        },
-        popup_input = {
-          border = {
-            highlight = "TelescopePromptBorder",
-          },
-          win_options = {
-            winhighlight = "Normal:TelescopePromptNormal,FloatBorder:FloatBorder",
-          },
-        },
-        settings_window = {
-          border = {
-            highlight = "TelescopePromptBorder",
-          },
-          win_options = {
-            winhighlight = "Normal:TelescopePromptNormal,FloatBorder:FloatBorder",
-          },
-        },
-        chat = {
-          sessions_window = {
-            border = {
-              highlight = "TelescopePromptBorder",
-            },
-            win_options = {
-              winhighlight = "Normal:TelescopePromptNormal,FloatBorder:FloatBorder",
-            },
-          },
-        },
-        help_window = {
-          border = {
-            highlight = "TelescopePromptBorder",
-          },
-          win_options = {
-            winhighlight = "Normal:TelescopePromptNormal,FloatBorder:FloatBorder",
-          },
-        },
-      }
-    end,
-    keys = {
-      { "<leader>;c", "<cmd>ChatGPT<cr>", desc = "ChatGPT" },
-      { "<leader>;a", "<cmd>ChatGPTActAs<cr>", desc = "ChatGPT: Act As" },
-      { "<leader>;C", "<cmd>ChatGPTCompleteCode<cr>", desc = "ChatGPT: Complete Code" },
-      {
-        "<leader>;e",
-        "<cmd>ChatGPTEditWithInstructions<cr>",
-        mode = { "n", "x" },
-        desc = "ChatGPT: Edit With Instructions",
-      },
-      { "<leader>;rg", "<cmd>ChatGPTRun grammar_correction<cr>", mode = { "n", "x" }, desc = "Grammar Correction" },
-      { "<leader>;rT", "<cmd>ChatGPTRun translate<cr>", mode = { "n", "x" }, desc = "Translate" },
-      { "<leader>;rk", "<cmd>ChatGPTRun keywords<cr>", mode = { "n", "x" }, desc = "Keywords" },
-      { "<leader>;rd", "<cmd>ChatGPTRun docstring<cr>", mode = { "n", "x" }, desc = "Docstring" },
-      { "<leader>;rt", "<cmd>ChatGPTRun add_tests<cr>", mode = { "n", "x" }, desc = "Add Tests" },
-      { "<leader>;ro", "<cmd>ChatGPTRun optimize_code<cr>", mode = { "n", "x" }, desc = "Optimize Code" },
-      { "<leader>;rs", "<cmd>ChatGPTRun summarize<cr>", mode = { "n", "x" }, desc = "Summarize" },
-      { "<leader>;rf", "<cmd>ChatGPTRun fix_bugs<cr>", mode = { "n", "x" }, desc = "Fix Bugs" },
-      { "<leader>;re", "<cmd>ChatGPTRun explain_code<cr>", mode = { "n", "x" }, desc = "Explain Code" },
-      { "<leader>;rR", "<cmd>ChatGPTRun roxygen_edit<cr>", mode = { "n", "x" }, desc = "Roxygen Edit" },
-      {
-        "<leader>;rr",
-        "<cmd>ChatGPTRun code_readability_analysis<cr>",
-        mode = { "n", "x" },
-        desc = "Code Readabiliy Analysis",
-      },
-    },
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
-  {
     "piersolenski/wtf.nvim",
     dependencies = {
       "MunifTanjim/nui.nvim",
@@ -165,6 +64,16 @@ return {
   },
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionChat", "CodeCompanionCmd" },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "openai",
+        },
+        inline = {
+          adapter = "openai",
+        },
+      },
+    },
   },
 }
