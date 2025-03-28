49099183c19c09463704572df1f307f0834762b3
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2025-03-28 16:47:20 -0700

    chore(astro): migrate selection ui to v5

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -149,22 +149,6 @@ return {
     },
   },
   {
-    "stevearc/dressing.nvim",
-    opts = {
-      select = {
-        backend = { "telescope", "builtin" },
-        telescope = {
-          layout_strategy = "horizontal",
-          layout_config = {
-            preview_cutoff = false,
-            width = function(_, max_columns, _) return math.min(max_columns, 80) end,
-            height = function(_, _, max_lines) return math.min(max_lines, 15) end,
-          },
-        },
-      },
-    },
-  },
-  {
     "stevearc/overseer.nvim",
     opts = {
       setup = {
