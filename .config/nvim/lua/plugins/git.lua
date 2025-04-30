4bec565fa277067a0aa80a5eb43ffa93558fe017
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2025-04-29 17:07:21 -0700

    chore(astro): remove vim-fugitive

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -3,19 +3,6 @@ local utils = require "astrocore"
 ---@type LazySpec
 return {
   {
-    "tpope/vim-fugitive",
-    event = "User AstroGitFile",
-    cmd = { "G", "Git", "GcLog", "Gclog", "GlLog", "Gllog" },
-    config = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        desc = "Make q close fugitive",
-        group = vim.api.nvim_create_augroup("quit_fugitive", { clear = true }),
-        pattern = { "fugitive", "fugitiveblame" },
-        callback = function() vim.keymap.set("n", "q", "<Plug>fugitive:gq", { buffer = true }) end,
-      })
-    end,
-  },
-  {
     "sindrets/diffview.nvim",
     event = "User AstroGitFile",
     opts = function()
