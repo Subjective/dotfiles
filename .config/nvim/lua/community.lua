51525639aa982cd66e880601499400b34186f722
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2026-04-10 21:32:26 -0700

    feat(astro): update language packs

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -14,7 +14,10 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.python.base" },
+  { import = "astrocommunity.pack.python.basedpyright" },
+  { import = "astrocommunity.pack.python.ruff" },
+  { import = "astrocommunity.pack.rust" },
 
   -- colorschemes
   { import = "astrocommunity.colorscheme.catppuccin" },
